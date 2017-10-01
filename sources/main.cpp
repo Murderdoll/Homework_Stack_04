173c540a86879a505afc5f59ad7edfafe1849518
Murderdoll <artem765@inbox.ru>
2017-10-02 00:58:44 +0300

    Update main.cpp

--- sources/main.cpp
+++ sources/main.cpp
@@ -11,8 +11,14 @@ void stackPush()
 {
 	cout << endl << "Метод stackPush" << endl;
 	Stack<int> arrayInt;
-	for(int i = 0; i < 10; i++) { arrayInt.push(i*2); }   
-	for(size_t j = 0; j < arrayInt.count(); j++) { cout << arrayInt[j] << " "; } // Заполняем наш стек и выводим его содержимое на экран
+	for (int i = 0; i < 10; i++) 
+	{ 
+		arrayInt.push(i*2);
+	}   
+	for (size_t j = 0; j < arrayInt.count(); j++) 
+	{ 
+		cout << arrayInt[j] << " "; 
+	} // Заполняем наш стек и выводим его содержимое на экран
 	cout << endl;
 }
 
@@ -20,19 +26,29 @@ void stackPop()
 {
 	cout << endl << "Метод stackPop" << endl;
 	Stack<int> arrayInt;
-	for(int i = 0; i < 10; i++) { arrayInt.push(i); }
-	for(int j = 0; j < 6; j++) { cout << arrayInt.pop() << " "; } // Заполняем наш стек и сразу же после этого извлекаем из него все элементы
+	for(int i = 0; i < 10; i++)
+	{ 
+		arrayInt.push(i);
+	}
+	for(int j = 0; j < 6; j++) 
+	{ 
+		cout << arrayInt.pop() << " ";
+	} // Заполняем наш стек и сразу же после этого извлекаем из него 6 элементов
 	cout << endl << endl;
 }
 
-void stackEmpty() 
+void stackEmpty()
 {
-	cout << endl << "Метод stackEmpty" << endl;
-	Stack<char> arrayChar;
-	char wordChar[] = "AABBCCDDEE";
-	for(int i = 0; i < strlen(wordChar); i++) { arrayChar.push(wordChar[i]); }
-	while(!arrayChar.empty()) {
-		cout << arrayChar.pop() << " ";
+	//cout << endl << "Метод stackEmpty" << endl;
+	Stack<int> Ob4;
+	for (int i = 0; i < 10; i++)
+        {
+		Ob4.push(i);
+		i++;
+        }
+	while (Ob4.empty() == 0)
+	{
+		cout << Ob4.pop() << " ";
 	}
 	cout << endl << endl;
 }
