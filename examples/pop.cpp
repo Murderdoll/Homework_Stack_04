1a64a3fe3abe164f07bf71edbef230bec9a144a2
Murderdoll <artem765@inbox.ru>
2017-10-05 05:56:17 +0300

    Update pop.cpp

--- examples/pop.cpp
+++ examples/pop.cpp
@@ -1,4 +1,21 @@
+#include <iostream>
 #include "Stack.hpp"
+using namespace std;
+
+void stackPop() 
+{
+	cout << endl << "Метод stackPop" << endl;
+	Stack<int> arrayInt;
+	for(int i = 0; i < 10; i++)
+	{ 
+		arrayInt.push(i);
+	}
+	for(int j = 0; j < 6; j++) 
+	{ 
+		cout << arrayInt.pop() << " ";
+	} // Заполняем наш стек и сразу же после этого извлекаем из него 6 элементов
+	cout << endl << endl;
+}
 
 int main() 
 {
