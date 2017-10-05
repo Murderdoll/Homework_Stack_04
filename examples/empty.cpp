4fae2a50206c50cb5521086709ad4c282df4ff8c
Murderdoll <artem765@inbox.ru>
2017-10-05 05:48:19 +0300

    Update empty.cpp

--- examples/empty.cpp
+++ examples/empty.cpp
@@ -1,4 +1,21 @@
+#include <iostream>
 #include "Stack.hpp"
+using namespace std;
+
+void stackEmpty()
+{
+	cout << endl << "Метод stackEmpty" << endl;
+	Stack<int> Ob4;
+	for (int i = 0; i < 10; i++)
+ {
+		Ob4.push(i);
+ }
+	while (Ob4.empty() == 0)
+	{
+		cout << Ob4.pop() << " ";
+	}
+	cout << endl << endl;
+}
 
 int main() 
 {
