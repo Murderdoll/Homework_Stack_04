2053416fca9d0eeb7398ae2bee0666b3f1911162
Murderdoll <artem765@inbox.ru>
2017-10-05 06:03:31 +0300

    Update main.cpp

--- sources/main.cpp
+++ sources/main.cpp
@@ -1,53 +1,37 @@
+#include <iostream>
 #include "Stack.hpp"
+using namespace std;
 
-void stackInit() 
+int main()
 {
-	cout << "Метод stackInit" << endl;
-	Stack<int> arrayInt;
-	cout << "Массив интов создан." << endl;
-}
-
-void stackPush()
-{
-	cout << endl << "Метод stackPush" << endl;
-	Stack<int> arrayInt;
-	for (int i = 0; i < 10; i++) 
-	{ 
-		arrayInt.push(i*2);
-	}   
-	for (size_t j = 0; j < arrayInt.count(); j++) 
-	{ 
-		cout << arrayInt[j] << " "; 
-	} // Заполняем наш стек и выводим его содержимое на экран
-	cout << endl;
-}
-
-void stackPop() 
-{
-	cout << endl << "Метод stackPop" << endl;
-	Stack<int> arrayInt;
-	for(int i = 0; i < 10; i++)
-	{ 
-		arrayInt.push(i);
-	}
-	for(int j = 0; j < 6; j++) 
-	{ 
-		cout << arrayInt.pop() << " ";
-	} // Заполняем наш стек и сразу же после этого извлекаем из него 6 элементов
-	cout << endl << endl;
-}
-
-void stackEmpty()
-{
-	cout << endl << "Метод stackEmpty" << endl;
-	Stack<int> Ob4;
-	for (int i = 0; i < 10; i++)
- {
-		Ob4.push(i);
- }
-	while (Ob4.empty() == 0)
+	try
 	{
-		cout << Ob4.pop() << " ";
+	Stack<int> Ob(3); // создаем объект класса Stack с размером 3
+	Ob.push(1);
+	Ob.push(2);
+	Ob.push(3);
+	Ob.push(4); 
+	for (int i = 0; i < 4; i++)
+	{
+		Ob.showMe(i);// заполняем наш стек и выодим его содержимое на экран
 	}
-	cout << endl << endl;
+	cout << endl;
+	Ob.pop(); // убираем верхний элемент из стека
+	for (int i = 0; i < 4; i++)
+	{
+		Ob.showMe(i); // убеждаемся, что элемент был убран
+	}
+	cout << endl;
+	Ob.pop();
+	Ob.pop();
+	Ob.pop(); // извлекаем оставшиеся три элемента из объекта
+	cout << "Now Stack is empty." << endl;
+	cout << Ob.empty() << endl;
+	//Ob.pop(); // Вызываем аварийную остановку программы, выходя за границы стека
+	}
+	catch (const exception& e)  	//обработка исключений
+	{
+		cout << e.what() << endl;
+	}
+	return 0;
 }
