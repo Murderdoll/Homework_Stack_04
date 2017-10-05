65e64475ef986b5b51d3ac3d1a127e2a6027a02e
Murderdoll <artem765@inbox.ru>
2017-10-05 05:56:49 +0300

    Update push.cpp

--- examples/push.cpp
+++ examples/push.cpp
@@ -1,4 +1,21 @@
+#include <iostream>
 #include "Stack.hpp"
+using namespace std;
+
+void stackPush()
+{
+	cout << endl << "Метод stackPush" << endl;
+	Stack<int> arrayInt;
+	for (int i = 0; i < 10; i++) 
+	{ 
+		arrayInt.push(i*2);
+	}   
+	for (size_t j = 0; j < arrayInt.count(); j++) 
+	{ 
+		cout << arrayInt[j] << " "; 
+	} // Заполняем наш стек и выводим его содержимое на экран
+	cout << endl;
+}
 
 int main() 
 {
