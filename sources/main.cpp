7b952c0359935721d1eec19a83531c4315593ebc
Murderdoll <artem765@inbox.ru>
2017-10-08 17:51:31 +0300

    Update main.cpp

--- sources/main.cpp
+++ sources/main.cpp
@@ -1,16 +1,36 @@
+#include <iostream>
 #include "Stack.hpp"
-
-void stackEmpty();
-void stackInit();
-void stackPop();
-void stackPush();
+using namespace std;
 
 int main()
 {
-	stackEmpty();
-        stackInit();
-        stackPop();
-        stackPush();
-	
+	try
+	{
+	Stack<int> Ob(3); // создаем объект класса Stack с размером 3
+	Ob.push(1);
+	Ob.push(2);
+	Ob.push(3);
+	Ob.push(4); 
+	for (int i = 0; i < 4; i++)
+	{
+		cout << Ob[i];// заполняем наш стек и выодим его содержимое на экран
+	}
+	cout << endl;
+	Ob.pop(); // убираем верхний элемент из стека
+	Ob.push(0); // помещаем на верхушку стека элемент 0
+	Ob.pop();
+	Ob.pop();
+	Ob.pop(); // извлекаем оставшиеся три элемента из объекта
+	for (int i = 0; i < 4; i++)
+	{
+		cout << Ob[i];
+	}
+	cout << endl;
+
+	}
+	catch (const exception& e)  	//обработка исключений
+	{
+		cout << e.what() << endl;
+	}
 	return 0;
 }
