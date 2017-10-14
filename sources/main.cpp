5cc6ab60faa16f952d1f867ef29572d413cb488a
Murderdoll <artem765@inbox.ru>
2017-10-14 17:31:50 +0300

    Update main.cpp

--- sources/main.cpp
+++ sources/main.cpp
@@ -7,30 +7,24 @@ int main()
 	try
 	{
 	Stack<int> Ob(3); // создаем объект класса Stack с размером 3
-	Ob.push(1);
-	Ob.push(2);
-	Ob.push(3);
-	Ob.push(4); 
 	for (int i = 0; i < 4; i++)
 	{
+		Ob.push(i+1);
 		cout << Ob[i];// заполняем наш стек и выодим его содержимое на экран
 	}
 	cout << endl;
 	Ob.pop(); // убираем верхний элемент из стека
 	Ob.push(0); // помещаем на верхушку стека элемент 0
-	Ob.pop();
-	Ob.pop();
-	Ob.pop(); // извлекаем оставшиеся три элемента из объекта
 	for (int i = 0; i < 4; i++)
 	{
 		cout << Ob[i];
 	}
 	cout << endl;
-
 	}
 	catch (const exception& e)  	//обработка исключений
 	{
 		cout << e.what() << endl;
 	}
+	system("pause");
 	return 0;
 }
