3997c5a4fd8f69df697d9d018c6ca9b55730557e
Murderdoll <artem765@inbox.ru>
2017-10-17 01:00:39 +0300

    Update tests.cpp

--- tests/tests.cpp
+++ tests/tests.cpp
@@ -1,7 +1,38 @@
 #include "catch.hpp"
 #include <Stack.hpp>
 
-SCENARIO("controle init") {
-	Stack<int> IntArr;
-	REQUIRE(IntArr.count() == 0);
+SCENARIO("controle init")
+{
+	Stack <int> Ob;
+	
+	REQUIRE(Ob.count() == 0);
+}
+
+SCENARIO("controle push")
+{
+	Stack <int> Ob;
+	
+	Ob.push(1);
+	
+	REQUIRE(Ob.count() == 1);
+}
+
+SCENARIO("controle pop")
+{
+	Stack <int> Ob;
+	
+        Ob.push(1);
+	Ob.pop();
+
+	REQUIRE(Ob.count() == 0);
+}
+
+SCENARIO("controle empty")
+{
+	Stack <int> Ob;
+
+	Ob.push(1);
+	Ob.pop();
+	
+	REQUIRE(Ob.empty() == true);
 }
